85840547c12e9f660802045ae0ea255b2b8743f2
IF_Add_Fahri <fadhlurrohman.fachri1@fiwa.sch.id>
2026-06-04 11:54:32 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+// sw.js - SiTASII Lab v1A Service Worker
+const CACHE_NAME='sitasiilab-v1a';
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(['/'])).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{if(!res||res.status!==200||res.type!=='basic')return res;const clone=res.clone();caches.open(CACHE_NAME).then(cache=>cache.put(e.request,clone));return res})))});
